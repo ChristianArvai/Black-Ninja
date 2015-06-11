319128d48f4508885bb91512ca5fdb952bf85764
ChristianArvai <ChristianArvai@users.noreply.github.com>
2015-06-11 16:05:48 +0200

    Update ubdk-main.sh

--- ubdk-main.sh
+++ ubdk-main.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # 
 # Author: Christian Arvai 
-# Credits to: Denmark LoCo Team
-# for all ubuntu releases from 12.04 to current stabil  
-# to both 32 and 64 bits systems
-# this code is available under the creative commons attribution-sharealike license https://creativecommons.org/licenses/by-sa/4.0/
+# Credits to: Ubuntu Denmark LoCo Team
+# For all Ubuntu releases from 12.04 to current stabil  
+# To 32 and 64 bits systems
+# This code is available under the creative commons attribution-sharealike license https://creativecommons.org/licenses/by-sa/4.0/
 
 # declare variable $select and assign value
 select="Please select [1,2,3,4,5,6,7,9,10,11,12 or 13]? "
