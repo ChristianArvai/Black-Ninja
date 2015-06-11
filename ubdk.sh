0f8d50d2af872578a60a8b8aa13df23ab6106eb5
ChristianArvai <ChristianArvai@users.noreply.github.com>
2015-06-11 16:12:23 +0200

    Update ubdk.sh

--- ubdk.sh
+++ ubdk.sh
@@ -1,1 +1,17 @@
+#!/bin/sh
+# 
+# Author: Christian Arvai 
+# Credits to: Ubuntu Denmark LoCo Team
+# For all Ubuntu releases from 12.04 to current stabil  
+# To 32 and 64 bits systems
+# This code is available under the creative commons attribution-sharealike license https://creativecommons.org/licenses/by-sa/4.0/
 
+echo ""
+echo "Updating Main menu"
+echo ""
+scriptname=ubdk-main.sh
+sudo rm -f /usr/local/bin/$scriptname;
+wget https://ubuntudanmark.dk/filer/ubdk-scripts/$scriptname -O ~/$scriptname && 
+chmod +x ~/$scriptname &&  
+sudo mv ~/$scriptname /usr/local/bin && 
+sudo $scriptname
