9d961d30be2277a8465f4e287712bf885890c554
ChristianArvai <ChristianArvai@users.noreply.github.com>
2015-06-14 01:55:10 +0200

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # 
+# Project: Black-Ninja
 # Author: Christian Arvai 
-# Credits to: Ubuntu Denmark LoCo Team
-# For all Ubuntu releases from 12.04 to current stabil  
-# To 32 and 64 bits systems
+# Credits: Ubuntu Denmark LoCo Team
+# For: All Ubuntu releases from 12.04 to current stabil  
+# To: 32 and 64 bits systems
 # This code is available under the creative commons attribution-sharealike license https://creativecommons.org/licenses/by-sa/4.0/
 
 sudo dpkg --configure -a &&
