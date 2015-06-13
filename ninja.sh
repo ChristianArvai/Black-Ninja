a715e952499b69ba43865d8556b50a02bce8d950
ChristianArvai <ChristianArvai@users.noreply.github.com>
2015-06-14 01:58:02 +0200

    Update ninja.sh

--- ninja.sh
+++ ninja.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 # 
+# Project: Black-Ninja
 # Author: Christian Arvai 
-# Credits to: Ubuntu Denmark LoCo Team
-# For all Ubuntu releases from 12.04 to current stabil  
-# To 32 and 64 bits systems
+# Credits: Ubuntu Denmark LoCo Team
+# For: All Ubuntu releases from 12.04 to current stabil  
+# To: 32 and 64 bits systems
 # This code is available under the creative commons attribution-sharealike license https://creativecommons.org/licenses/by-sa/4.0/
 
+# declare variable $https (path to server) and assign value
+https=https://ubuntudanmark.dk/filer/ninja
+# declare variable $bin (path to folder /usr/local/bin) and assign value
+bin=usr/local/bin
+
 echo ""
-echo "Updating Main menu"
+echo "Updating Black-Ninja main menu"
 echo ""
-scriptname=ubdk-main.sh
-sudo rm -f /usr/local/bin/$scriptname;
-wget https://ubuntudanmark.dk/filer/ubdk-scripts/$scriptname -O ~/$scriptname && 
+scriptname=ninja-main.sh
+sudo rm -f /$bin/$scriptname;
+wget $https/$scriptname -O ~/$scriptname && 
 chmod +x ~/$scriptname &&  
-sudo mv ~/$scriptname /usr/local/bin && 
-sudo $scriptname
+sudo mv ~/$scriptname /$bin && 
+$scriptname
